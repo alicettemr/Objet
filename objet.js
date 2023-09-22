39293322af800bd41ac87257ddfeee612bea43eb
Alice <145458400+alicettemr@users.noreply.github.com>
2023-09-22 15:59:20 +0200

    Add files via upload

---
+++ objet.js
@@ -0,0 +1,8 @@
+let student={
+    name:'Nicolas',
+    favoriteFood:"Salad",
+    city:"Paris",
+}
+let values=Object.values(student);
+let count=values.reduce((acc,value)=>acc+value.length,0)
+console.log(count);
